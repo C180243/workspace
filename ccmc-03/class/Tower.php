d4b9a7dcf8001d7d07361431bf3e0a2b6f4cf131
C180243 <c180243@ccg.ac.jp>
2019-11-11 03:22:56 +0000

    2019-11-11

---
+++ ccmc-03/class/Tower.php
@@ -0,0 +1,22 @@
+<?php
+require_once("Building.php");
+class Tower extends Building {
+    /**
+     * 属性
+     */
+     private $builtYear;
+     
+     /**
+      * コンストラクタ
+      */
+      function __construct(string $name, int $builtYear) {
+          parent::__construct($name, $height);
+          $this->builtYear = $builtYear;
+          
+      }
+      function getProfile():string {
+          $profile = "{$this->name} の高さは{$this->height} mで、{$this->builtYear} 年に開業しました。";
+          return $profile;
+      }
+}
+?>
